0c8f19b8deaaaa4d5bfbf66ae8f60cfa4f723b38
jzadroga <jason.zadroga@gmail.com>
2016-07-06 13:02:05 -0400

    adding class libraries

---
+++ WHGSchedulerSite/Models/SponsorModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WHGSchedulerSite.Models
+{
+    public class SponsorModel
+    {
+        public string logoImage { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public int sponsorID { get; set; }
+    }
+}
---
+++ WHGSchedulerSite/ViewModels/SponsorsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WHGSchedulerSite.Models;
+
+namespace WHGSchedulerSite.ViewModels
+{
+    public class SponsorsViewModel
+    {
+        public List<SponsorModel> sponsorList { get; set; }
+
+        public SponsorsViewModel()
+        {
+            this.sponsorList = new List<SponsorModel>();
+        }
+    }
+}
